43263ad75ae767acb8162e87e552972e69d53069
Jean van Niekerk <jean.jacque@rocketmail.com>
2020-01-20 13:28:25 +0200

    devlogger with components and service set up

---
+++ src/app/services/log.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, of } from "rxjs";
+
+import { Log } from "../models/log";
+
+@Injectable({
+  providedIn: "root"
+})
+export class LogService {
+  logs: Log[];
+
+  private logSource = new BehaviorSubject<Log>({
+    id: null,
+    text: null,
+    date: null
+  });
+
+  selectedLog = this.logSource.asObservable();
+
+  constructor() {
+    this.logs = [
+      {
+        id: "1",
+        text: "Generated components",
+        date: new Date("12/26/2017 12:54:23")
+      },
+      {
+        id: "2",
+        text: "Added bootstrap",
+        date: new Date("12/28/2017 12:54:23")
+      },
+      {
+        id: "3",
+        text: "Added logs component",
+        date: new Date("12/29/2017 12:54:23")
+      }
+    ];
+  }
+
+  getLogs(): Observable<Log[]> {
+    return of(this.logs);
+  }
+
+  setFormLog(log: Log) {
+    this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if (log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+  }
+}
